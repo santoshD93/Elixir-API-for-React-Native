b487a3cf4e1483f6a2f9c79f60f26dbacf2cec69
khubaib <khubaibahmad1994@gmail.com>
2021-06-12 13:46:29 +0200

    added posts schema, migration and tests

---
+++ backend/lib/backend/posts/post.ex
@@ -0,0 +1,20 @@
+defmodule Backend.Posts.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "posts" do
+    field :title, :string
+    field :body, :string
+    field :status, Ecto.Enum, values: [:unpublished, :published], default: :unpublished
+    field :published_at, :utc_datetime
+
+    timestamps(inserted_at: :created_at)
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+end
---
+++ backend/lib/backend/posts/posts.ex
@@ -0,0 +1,52 @@
+defmodule Backend.Posts do
+  import Ecto.Query, warn: false
+  alias Backend.Repo
+
+  alias Backend.Posts.Post
+
+  @doc """
+  Returns the list of posts.
+  """
+  def list_posts do
+    Repo.all(Post)
+  end
+
+  @doc """
+  Gets a single post.
+
+  Raises `Ecto.NoResultsError` if the Post does not exist.
+  """
+  def get_post!(id), do: Repo.get!(Post, id)
+
+  @doc """
+  Creates a post.
+  """
+  def create_post(attrs \\ %{}) do
+    %Post{}
+    |> Post.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a post.
+  """
+  def update_post(%Post{} = post, attrs) do
+    post
+    |> Post.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a post.
+  """
+  def delete_post(%Post{} = post) do
+    Repo.delete(post)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking post changes.
+  """
+  def change_post(%Post{} = post, attrs \\ %{}) do
+    Post.changeset(post, attrs)
+  end
+end
